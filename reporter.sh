7afdfdca9d50935cdbf4199be1b5746715502983
Tota77 <rafavg77@gmail.com>
2017-04-01 17:57:26 -0600

    Update reporter.sh

--- reporter.sh
+++ reporter.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
-
 players=$(/usr/local/bin/minecraft_cli command list | awk "NR==3 {print $3}" | cut -d ":" -f 4)
 
-if [ -z "$players" ]
+if [[ -z "${players// }" ]]
 then
-	/usr/local/bin/telegram_bot_players "$players"
-	#echo "No players Connected"
-else 
-	#/usr/local/bin/telegram_bot "$players"
-	echo "No players Connected"
+        #/usr/local/bin/telegram_bot_players "$players"
+        echo "No players Connected"
+else
+        /usr/local/bin/telegram_bot_players "$players"
+        echo "players Connected: $players"
 fi
